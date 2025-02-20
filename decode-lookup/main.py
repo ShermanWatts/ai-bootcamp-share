88f3d899f701546776af860f509b121877a9e69c
ShermanWatts <sherman.watts@gmail.com>
2025-02-19 16:10:12 -0800

    update

--- decode-lookup/main.py
+++ decode-lookup/main.py
@@ -1,9 +1,12 @@
+"""
+Example from discussion from class - use dict to lookup encoded values (to decode)
+"""
 
 import pandas as pd
 
 #csv-like data
 raw_data = [
-    ['sex','education'],
+    ['gender','education'],
     [0,0],
     [1,1],
     [0,2],
@@ -20,20 +23,20 @@ df = pd.DataFrame(raw_data,columns=column_headers)
 
 #lookup dict to decode values
 lookup = {
-    'sex': {0: 'Male', 1: 'Female'},
+    'gender': {0: 'Male', 1: 'Female'},
     'education': {0: 'High School', 1: 'College', 2: 'Post Graduate'}
 }
 
 #do some processing and lookup actual values if you ever need to
 for row_num in range(1, len(df)):  # Start from index 1 instead of 0
-    sex_val = df.iloc[row_num, 0] 
+    gender_val = df.iloc[row_num, 0] 
     education_val = df.iloc[row_num, 1] 
 
     # Encoded values
-    print(f"Row {row_num}: {sex_val}, {education_val}")
+    print(f"Row {row_num}: {gender_val}, {education_val}")
 
     # Decode values using the lookup dictionary
-    sex_text = lookup[df.columns[0]][sex_val]  # Lookup for 'sex'
+    gender_text = lookup[df.columns[0]][gender_val]  # Lookup for 'gender'
     education_text = lookup[df.columns[1]][education_val]  # Lookup for 'education'
     
-    print(f"Row {row_num}: {sex_text}, {education_text}")
+    print(f"Row {row_num}: {gender_text}, {education_text}")
